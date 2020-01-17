ec833d2e01bd9a8df891ba7ed8cefc55666d6d45
yebin <yebin@ppdai.com>
2020-01-17 16:53:06 +0800

    优化切换master分支

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 const execSync = require('child_process').execSync;
 const program = require('commander'); // 命令参数处理
-program.parse(process.argv);
+const version = require('./package').version
+program.version(version).parse(process.argv);
 let branchName = process.argv[2];
 if (!branchName) {
     console.log('请输入版本号')
@@ -14,7 +15,8 @@ if (!branchName) {
         if(str.includes('_' + branchName + '_')) {
             console.log('已存在同名分支，请删除相应分支或重新定义版本号')
         } else {
-            execSync('git checkout origin/master', {encoding: 'utf8'});
+            execSync('git checkout master', {encoding: 'utf8'});
+            execSync('git pull', {encoding: 'utf8'});
             execSync('git checkout -b DEV_' + branchName + '_BR', {encoding: 'utf8'});
             execSync('git push --set-upstream origin DEV_' + branchName + '_BR', {encoding: 'utf8'});
             execSync('git checkout -b TEST_' + branchName + '_BR', {encoding: 'utf8'});
